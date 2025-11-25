6d492abcb04c2e176708f2e48aaca7b5d5f2b10b
saviour0803 <70982630+saviour0803@users.noreply.github.com>
2025-11-25 20:28:53 +0100

    Modify bot.py environment variable assignments
    Updated environment variable values for bot configuration.

--- bot.py
+++ bot.py
@@ -6,10 +6,10 @@ from datetime import datetime
 import os
 
 # Correct way — secrets from Render env vars
-BOT_TOKEN = os.environ '8214837266:AAHLVEX6j0l6-zuiKWM4GA1ker-Ery4jd5w'
-VERIFICATION_API_URL = os.environ 'https://api.ninslip.com/verify'
-API_KEY = os.environ 'b59bb13b7d445b79ad1bfd336c55d4ba507e47f5db8e506be46ef9b14c1ca3e7'
-CHANNEL_ID = os.environ '-1002771187567'
+BOT_TOKEN = os.environ '8214837266:AAHLVEX-Ery4jd5w'
+VERIFICATION_API_URL = os.environ 'https://api
+API_KEY = os.environ 6be46ef9b14c1ca3e7'
+CHANNEL_ID = os.environ '-10
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
